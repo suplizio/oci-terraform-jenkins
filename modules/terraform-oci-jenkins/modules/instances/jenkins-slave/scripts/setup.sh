50c4567f1e542f4b28db0aaa75963c4ccc15ea33
Jason Suplizio <jason.suplizio@oracle.com>
2018-10-14 21:16:35 -0700

    writing slave sshkey to local

--- modules/terraform-oci-jenkins/modules/instances/jenkins-slave/scripts/setup.sh
+++ modules/terraform-oci-jenkins/modules/instances/jenkins-slave/scripts/setup.sh
@@ -19,11 +19,21 @@ wget -P /tmp ${jenkins_master_url}/jnlpJars/slave.jar
 sudo mv /tmp/slave.jar /home/jenkins/jenkins-slave/
 
 # CodeOne demo
+
+# CodeOne demo
 sudo yum install -y terraform terraform-provider-oci git ansible
 sudo -u opc ssh-keygen -t rsa -b 4096 -N "" -f /home/opc/.ssh/id_rsa
 chown opc:opc /home/opc/.ssh/id_rsa
 chmod 0600 /home/opc/.ssh/id_rsa
 
-sudo -u jenkins ssh-keygen -t rsa -b 4096 -N "" -f /home/jenkins/.ssh/id_rsa
-chown opc:opc /home/jenkins/.ssh/id_rsa
-chmod 0600 /home/jenkins/.ssh/id_rsa
+sudo -u jenkins mkdir /var/lib/jenkins/.ssh
+sudo -u jenkins ssh-keygen -t rsa -b 4096 -N "" -f /var/lib/jenkins/.ssh/id_rsa
+sudo -u jenkins ssh-keyscan -t rsa github.com >> /var/lib/jenkins/.ssh/known_hosts
+sudo chown jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
+sudo chown jenkins.jenkins /var/lib/jenkins/.ssh/known_hosts
+sudo chmod 0600 /var/lib/jenkins/.ssh/id_rsa
+
+sudo mkdir /var/lib/jenkins/workspace/
+sudo mkdir /var/lib/jenkins/workspace/tf_files
+sudo chown -R jenkins:jenkins /var/lib/jenkins/workspace/
+sudo chown -R jenkins:jenkins /var/lib/jenkins/workspace/tf_files
